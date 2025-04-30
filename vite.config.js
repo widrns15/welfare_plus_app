fd22cbe9240de3eb392a5d6f6fec19618ef7a3ef
il <viego41010@gmail.com>
2025-04-30 17:16:57 +0900

    Github Actions 배포 세팅

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: "/welfare_plus_app/",
     plugins: [react()],
     optimizeDeps: {
         include: ["styled-components"],
