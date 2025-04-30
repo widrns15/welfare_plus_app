58785f3bce5b223710a76f08302853496ef47847
il <viego41010@gmail.com>
2025-04-30 18:05:37 +0900

    fix: relative script for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     base: "/welfare_plus_app/",
+    pagesDir: [
+        {
+            dir: "src/pages",
+            baseRoute: "/welfare_plus_app",
+        },
+    ],
     plugins: [react()],
     optimizeDeps: {
         include: ["styled-components"],
